f2411ff8252b6672706a11ed8fb0c4ee5f4474db
agauchat <augusto.gauchat@mercadolibre.com>
2020-10-14 00:26:35 -0300

    new activity

---
+++ app/src/main/java/com/example/android/trickcount/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.android.trickcount
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.trickcount.R.layout.main_activity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(main_activity)
+    }
+}
